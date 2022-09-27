97064c0aa057fd807daac6c57c02ab92485a81fd
ospite <ospite@INF01PC091>
2022-09-27 12:07:08 +0200

    Aggiungere i file di progetto.

---
+++ MarkoCoboEs1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MarkoCoboEs1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string linea;
+            var contatto = new string[2];
+            try
+            {
+                StreamReader sr = new StreamReader("rubrica.txt");
+                linea = sr.ReadLine();
+                contatto = linea.Split(',');
+                do
+                {
+                    Console.WriteLine("NOME: " + contatto[0] + " COGNOME" + contatto[1] +   " NUMERO" + contatto[2]);
+                    linea = sr.ReadLine();
+
+                } while (linea != null);
+
+                    sr.Close();
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception" + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block. ");
+            }
+        }
+    }
+}
